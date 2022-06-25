2587fa70771db8a17b7aaf2c15a0205dfca736f2
meharsh2001 <94461844+meharsh2001@users.noreply.github.com>
2022-06-25 23:08:42 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
+const user = require("./models/user");
+
 var express         =require("express"),
     mongoose        =require("mongoose"),
-    port            =process.env.PORT || 8000,
+    port            =process.env.PORT || 3001,
     app             =express(),
-    User            =require("./models/user")
+
+    methodOverride  =require("method-override")
+    User            =require("./models/user"),
+    bodyParser      =require("body-parser")
+
+    app.use(methodOverride("_method"));
+    app.use(bodyParser.urlencoded({extended:false}));
+
 app.use(express.static(__dirname + "/public"));
 app.set("view engine","ejs");
 
-app.get("/",function(req,res)
-{
-    res.render("home");
-    const newUser=new User({ 
-      });newUser.save(); 
-});; 
-
 
 //ONLINE MONGODB ATLAS
-const DB='mongodb+srv://admin:admin@cluster0.oun5l.mongodb.net/MissingX';
+const DB='mongodb+srv://admin:admin@cluster0.oun5l.mongodb.net/assignment';
 mongoose.connect(DB).then(()=>{console.log('DATABASE CONNECTED to localhost:'+ port);}).catch((err)=> console.log('Database Not Connected'+err));
 
 app.use(require("express-session")({
@@ -23,6 +25,44 @@ app.use(require("express-session")({
     resave:false,
     saveUninitialized:false
 }));
+
+
+app.get("/",function(req,res)
+{   
+    User.find({},function(err,user){
+    res.render("home",{userlist: user})});
+});;
+
+app.post("/",function(req,res){ 
+    const newuser=new user({ 
+    name:req.body.name,
+    phone:req.body.number,
+    email:req.body.email, 
+     books:[{
+         "sno": req.body.srno,
+         "booktitle": req.body.title,
+         "author": req.body.author,
+         "genre": req.body.genre,
+         "yop": req.body.year,
+         "isbm": req.body.isbm
+     }]
+    });newuser.save();
+    res.redirect("/");});
+
+
+app.post("/delete",function(req,res)
+{  console.log(req.body.id);
+     User.findByIdAndRemove(req.body.id,(err) => {
+    if(err)
+    {     console.log(err); 
+    }});
+    res.redirect("/");
+});
+
+app.post("/update",function(req,res)
+{   console.log(req.body.id);
+    User.findByIdAndUpdate(req.body.id,{books:[{sno:req.body.sno,booktitle:req.body.booktitle,author:req.body.author,genre:req.body.genre,yop:req.body.yop,isbm:req.body.isbm}]}, function(err, data) {
+        if(err){console.log(err);}else{res.redirect("/");}});});
 
 app.listen(port,function()
 {
--- models/user.js
+++ models/user.js
@@ -1,16 +1,24 @@
 var mongoose=require("mongoose");
 
 var UserSchema=new mongoose.Schema({
-   profilepic:String,
-   firstname:String,
-   lastname:String,
-   number:String,
-   dob:String,
-    gender:String,
-    username:String,
-    password:String,
-    paid:{type:Boolean,default:false},
-    isAdmin:{type:Boolean,default:false}
+   name:String,
+   phone:String,
+   email:String, 
+    books:{type:[{
+        "sno": String,
+        "booktitle": String,
+        "author": String,
+        "genre": String,
+        "yop": String,
+        "isbm": String
+    }],default:[{
+        "sno": "0",
+        "booktitle": "nul",
+        "author": "nul",
+        "genre": "nul",
+        "yop": "nul",
+        "isbm": "nul"
+    }]},
 });
 //var log= mongoose.model("log",logSchema);
 module.exports= mongoose.model("User",UserSchema);
